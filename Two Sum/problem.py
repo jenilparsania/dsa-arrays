ed4671816672fe48c4deebd67bcbb40b135e00f1
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-02-09 17:19:13 -0400

    2Sum brute force

---
+++ Two Sum/problem.py
@@ -0,0 +1,45 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+
+def TwoSum(nums:list,target):
+    for i in range(0,len(nums)):
+        answer = 0
+        for j in range(i+1,len(nums)):
+            answer = nums[i] + nums[j]
+            if answer == target :
+                return [i,j]
+        
+    
+    return("no solution")
+
+
+    pass
+
+nums = [2,7,11,15]
+target = 17
+
+print(TwoSum(nums,target))
+
+
+
