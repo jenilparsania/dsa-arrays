1a21d706d5e7af57f45c622d384f54c5c2b8c1bb
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-02-10 23:04:11 -0400

    2Sum brute-force-2

---
+++ Longest Subarray with Sum K/brute-force2.py
@@ -0,0 +1,18 @@
+def LongestSubArray(nums:list,target:int)->int:
+
+    n = len(nums)
+    length = 0
+    for i in range(0,n):
+        add = 0
+        
+        for j in range(i,n):
+            add += nums[j]
+            if add == target:
+                length = max(length,j-i+1)
+   
+    return length
+    pass
+
+arr = [-5, 8, -14, 2, 4, 12]
+k = -5
+print(LongestSubArray(arr,k))
