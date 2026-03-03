96f41ad82c175d6c5c095b615d8e967c5b47b8f9
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-03-03 18:00:35 -0400

    better

---
+++ Longest Consecutive Sequence/better.py
@@ -0,0 +1,28 @@
+
+#  the solution is of sorting the array and then checking for longest sequence
+def LongestConsSeq(nums:list):
+    nums.sort()
+    last_smaller = float("-inf")
+    count=1
+    longest = 0
+    n = len(nums)
+    for i in range(0,n):
+        if nums[i] - last_smaller > 1:
+            longest = max(count,longest)
+            last_smaller = nums[i]
+            count=1
+        elif nums[i]-last_smaller ==1:
+            count+=1
+            last_smaller = nums[i]
+        
+        longest = max(count,longest)
+        
+    return longest
+
+
+
+    pass
+
+nums = [1,99,101,98,2,5,3,100,1]
+
+print(LongestConsSeq(nums))
