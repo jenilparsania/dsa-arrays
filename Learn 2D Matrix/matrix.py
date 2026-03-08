660f8740047bb571401d7eda3e5b747351c1efeb
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-03-08 16:23:45 -0300

    iterating matrix

--- Learn 2D Matrix/matrix.py
+++ Learn 2D Matrix/matrix.py
@@ -3,3 +3,13 @@
 # nums = [[5,20,3],[7,-10,9],[1,-52,6]]
 # rows = len(nums)
 # columns = len(nums[0])
+
+# iterating
+nums = [[5,20,3],[7,-10,9],[1,-52,6]]
+# rows : i , columns : j
+
+rows = len(nums)
+cols = len(nums[0])
+for i in range(0,rows):
+    for j in range(0,cols):
+        print(nums[i][j])
