eac73f218d8e15d06a2e2709a1dbdea7088871e6
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-03-16 15:01:33 -0300

    brute

--- Set Matrix Zeros/problem.py
+++ Set Matrix Zeros/problem.py
@@ -16,16 +16,44 @@ def printMatrix(nums):
 # second iteration for changing the infis to zero
 
 
-nums = [[7,8,3],[17,18,13],[27,0,23]]
-tracker = []
-printMatrix(nums)
-rows = len(nums)
-cols = len(nums[0])
 
-for i in range(rows):
-    for j in range(cols):
-        if nums[i][j] == 0:
-            tracker.append([i,j])
 
-printMatrix(tracker)
+
+def setZeros(matrix)->None:
+    def makeInfinity(matrix,row,col):
+        r = len(matrix)
+        c = len(matrix[0])
+        for i in range(0,r):
+            if matrix[i][col] != 0:
+                matrix[i][col] = float("inf")
+        for j in range(0,c):
+            if matrix[row][j] != 0:
+                matrix[row][j] = float("inf")
+        
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    for i in range(0,rows):
+        for j in range(0,cols):
+            if matrix[i][j] == 0:
+                makeInfinity(matrix,i,j)
+
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == float("inf"):
+                matrix[i][j] = 0
+    
+    return matrix
+
+
+nums = [[1,2,3],[11,12,13],[21,0,23]]
+print(setZeros(nums))
+
+"""
+TC -> O((N+M)*(N*M)) + O(N*M) 
+SC -> O(1)
+"""
+    
+
 
